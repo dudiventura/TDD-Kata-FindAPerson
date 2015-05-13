8e078b23170a2675b3c7405331445ec3754cf0c7
dudiventura <dudiventura@gmail.com>
2015-05-13 14:38:19 +0300

    Green-first feature

--- FindPersonTests.py
+++ FindPersonTests.py
@@ -9,16 +9,16 @@ class FindPersonTests(unittest.TestCase):
 									"Lassy Come Home"])
 		
 	def test_getAllPostsForName(self):
-		posts = crowdmap.get_all_posts_for("Or")
-		self.assertEquals(["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "MissingCowboy"])
+		posts = self.crowdmap.get_all_posts_for("Or")
+		self.assertEquals(posts, ["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley"])
 
 	def test_getAllPostForMissingName(self):
 		posts = self.crowdmap.get_all_posts_for("Joe")
 		self.assertEquals([], posts)
 		
-		def test_existingLocationInformationReturnsTrue(self):
-			location_exist = self.crowdmap.is_location_for_name("Or")
-			self.AssertTrue(location_exist)
+	def test_existingLocationInformationReturnsTrue(self):
+		location_exist = self.crowdmap.is_location_for_name("Or")
+		self.assertTrue(location_exist)
 		
 if __name__ == '__main__':
 	unittest.main()
